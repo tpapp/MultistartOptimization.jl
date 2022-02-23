d81b51d4b8adf744d01d24387573cf9c65b986fc
Tyler Beason <tbeason@users.noreply.github.com>
2022-02-23 03:04:42 -0500

    allow for differentiable objective functions and local gradient-based methods (#27)
    allow for differentiable objective functions and local gradient-based methods + tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using MultistartOptimization
 using Test
 using NLopt: NLopt
+using ForwardDiff
 
 include("test_functions.jl")
 
@@ -36,4 +37,36 @@ end
     p = multistart_minimization(TikTak(100), _local_method, P)
     @test p.location == x0
     @test p.value == 0.0
+end
+
+
+@testset "local gradient-based methods" begin
+    function autodiff(fn)
+        # adapted from here https://github.com/JuliaOpt/NLopt.jl/issues/128
+        function f(x) 
+            return fn(x) 
+        end
+    
+        function f(x,∇f)
+            if !(∇f == nothing) && (length(∇f) != 0)
+                ForwardDiff.gradient!(∇f,fn,x)
+            end
+        
+            fx = fn(x)
+            return fx
+        end
+        return f
+    end
+
+    for F in setdiff(TEST_FUNCTIONS, (RASTRIGIN, )) # Rastrigin disabled for now
+        n = 10
+        P = MinimizationProblem(autodiff(F), lower_bounds(F, n), upper_bounds(F, n))
+        local_method = NLoptLocalMethod(NLopt.LD_LBFGS)
+        multistart_method = TikTak(100)
+        p = multistart_minimization(multistart_method, local_method, P)
+        x₀ = minimum_location(F, n)
+        @test p.location ≈ x₀ atol = 1e-5
+        @test p.value ≈ F(x₀) atol = 1e-10
+    end
+
 end
